c3e9dc6e50bb17cb96ee79965aacd92d05eaf1fc
miniengineer <samadova.minira@gmail.com>
2022-02-26 10:11:28 +0900

    implement LinkedListCycle with two pointers: slow and fast

--- src/LinkedListCycle.java
+++ src/LinkedListCycle.java
@@ -1,10 +1,4 @@
-//Given head, the head of a linked list, determine if the linked list has a cycle in it.
-//
-//    There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer.
-//    Internally, pos is used to denote the index of the node that tail's next pointer is connected to.
-//    Note that pos is not passed as a parameter.
-//
-//    Return true if there is a cycle in the linked list. Otherwise, return false.
+//    Link:
 //
 //    Example 1:
 //
@@ -35,7 +29,7 @@ public class LinkedListCycle {
         // must be true
     }
 
-    // Time complexity is O(n)
+    // Time complexity is O(n) - we iterate over linkedlist once
     // Space complexity is O(1) - cause we don't use extra space,
     //                            only two constant pointers
     public static boolean hasCycle(ListNode head) {
@@ -48,9 +42,8 @@ public class LinkedListCycle {
         ListNode slow = head;
         ListNode fast = head.next;
 
-        // there is no point to check if slow != null
-        // because fast is one step ahead of slow
-        // and if fast is not null, then slow is 100% not null
+        // there is no point to check snow for null value
+        // slow is the previous value of fast which we check for not being null
         while (fast != null && fast.next != null) {
             if (slow == fast) return true;
 
